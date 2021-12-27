df73bd0c264f63d1d02a9a69318bdb6c2cbe971e
Nitin Diwakar <diwakarnitin509@gmail.com>
2021-12-27 13:26:19 +0530

    Code1 is added

---
+++ Code1.py
@@ -0,0 +1,17 @@
+# set of number and sum is given
+# -1,1,0,0,-2,2
+
+# sum = 0
+# print no. of combination possible of lenght 4
+# which sum = 0
+from itertools import combinations
+num = list(map(int,input().split(',')))
+s = int(input())
+com = list(combinations(num,4))
+
+count = 0
+for i in com:
+    i = sum(i)
+    if(i == s):
+        count+=1
+print(count)
